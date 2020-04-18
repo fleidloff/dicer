ef9cef0d67b527a0f75d229ef9dff138c8588857
Frederik Leidloff <kontakt@frederik-leidloff.de>
2020-04-18 23:00:40 +0200

    add meta info

--- pages/api/test.js
+++ pages/api/test.js
@@ -7,6 +7,13 @@ export default (req, res) => {
 	pusher.trigger('my-channel', 'my-event', {
   		"message": "hello world"
 	});
+	pusher.get({ path: '/channels/my-channel', params: {} }, function(error, request, response) {
+  if(response.statusCode === 200) {
+    var result = JSON.parse( response.body );
+    var channelsInfo = result.occupied;
+    console.log(channelsInfo)
+  }
+});
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({ name: 'John Doe' }))
