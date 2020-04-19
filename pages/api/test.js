bddbcffdb788496a39d583e75752448b26ce8532
Frederik Leidloff <kontakt@frederik-leidloff.de>
2020-04-19 14:35:16 +0200

    try awaiting pusher result

--- pages/api/test.js
+++ pages/api/test.js
@@ -14,18 +14,33 @@ const secretSettings = {
 
 const pusher = new Pusher({ ...secretSettings, ...publicSettings });
 
-export default (req, res) => {
-	pusher.trigger('my-channel', 'my-event', {
-  		"message": "hello world"
-	});
-	pusher.get({ path: '/channels/my-channel', params: {} }, function(error, request, response) {
-  if(response.statusCode === 200) {
-    var result = JSON.parse( response.body );
-    var channelsInfo = result.occupied;
-    console.log(channelsInfo)
-  }
-});
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ some: "result" }))
+export default async function(req, res) {
+	let promise = new Promise((resolve, reject) => {
+    	const timeout = setTimeout(() => {
+			res.statusCode = 200
+			res.setHeader('Content-Type', 'application/json')
+	  		res.end(JSON.stringify({ some: "timeout" }))
+		}, 3000);
+
+		pusher.trigger('my-channel', 'my-event', {
+	  		"message": "hello world"
+		}, () => {
+			clearTimeout(timeout);
+			resolve();
+		});
+  	});
+
+	await promise;
+
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.end(JSON.stringify({ some: "result" }))
+
+	/*pusher.get({ path: '/channels/my-channel', params: {} }, function(error, request, response) {
+	  if(response.statusCode === 200) {
+	    var result = JSON.parse( response.body );
+	    var channelsInfo = result.occupied;
+	    console.log(channelsInfo)
+	  }
+	});*/
 }
