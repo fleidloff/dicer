425e4ed416c39c15eb4abf7b49a3eccea242d2a6
Frederik Leidloff <kontakt@frederik-leidloff.de>
2020-05-01 22:05:30 +0200

    remove obsole numberDie

--- components/Dice/NumberDie.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import styles from "./dice.module.css"
-
-export default function Die({ value, color }) {
-  return <div className={`${styles.die} ${styles[color]}`}>{value || "?"}</div>
-}
