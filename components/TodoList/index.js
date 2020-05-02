4141d9e6c1359456bd80240b970b54b91c031437
Frederik Leidloff <kontakt@frederik-leidloff.de>
2020-05-02 16:07:18 +0200

    rename grid-areas in order to prevent automatic renaming by webpack (red -> #f00)

--- components/TodoList/index.js
+++ components/TodoList/index.js
@@ -2,6 +2,7 @@ import { store } from "react-recollect"
 import TodoList from "./TodoList"
 
 store.todos = [
+  "check why css grid is not working in dicer app",
   "locally use websockets instead of pusher (because concurrent connections)",
   "cleanup Qwixx.js -> extract user stuff",
   "dice + dice rolling design",
